b73e02f663834a67834ed90f7af3d0afa0eb9a4b
phuongtoan521h0486 <521h0486@student.tdtu.edu.vn>
2025-01-10 22:24:52 +0700

    update flexible base url

--- lib/res/widgets/app_urls.dart
+++ lib/res/widgets/app_urls.dart
@@ -1,9 +1,11 @@
+import 'dart:io' show Platform;
+
 class AppUrls {
-  static const baseUrl = "http://192.168.8.129:8080/api";
-  static const loginEndPoint = "$baseUrl/user/sign-in";
-  static const registerEndPoint = "$baseUrl/user/sign-up";
-  static const sendOTP = "$baseUrl/user/getOTP";
-  static const verifyOTP = "$baseUrl/user/verifyOTP";
+  static final baseUrl = "http://${Platform.isAndroid ? "10.0.2.2" : "localhost"}:8080/api";
+  static final loginEndPoint = "$baseUrl/user/sign-in";
+  static final registerEndPoint = "$baseUrl/user/sign-up";
+  static final sendOTP = "$baseUrl/user/getOTP";
+  static final verifyOTP = "$baseUrl/user/verifyOTP";
 
   static var moviesBaseUrl =
       'https://dea91516-1da3-444b-ad94-c6d0c4dfab81.mock.pstmn.io/';
